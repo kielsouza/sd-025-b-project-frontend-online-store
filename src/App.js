593b6cdeb4d3f9672f0b1b77edfeff08c565abf4
Eduardo de Lima Balbinot <914ThePro@gmail.com>
2022-10-07 17:26:24 -0300

    requisito 3 feito

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Link, Route, Switch } from 'react-router-dom';
 import './App.css';
+import carrinhoDeCompras from './pages/carrinhoDeCompras';
 import productList from './pages/productList';
 
 function App() {
   return (
     <BrowserRouter>
       <Switch>
+        <Route path="/carrinho" component={ carrinhoDeCompras } />
         <Route path="/" component={ productList } exact />
       </Switch>
+
+      <Link to="/carrinho" data-testid="shopping-cart-button">Carrinho de compras</Link>
     </BrowserRouter>
   );
 }
