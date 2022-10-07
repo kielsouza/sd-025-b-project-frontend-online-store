e16a4b69c3ac1eb46af9b1e6b8f89b5bcee65fdf
alexandrebelem21 <alexandrebelem21@gmail.com>
2022-10-07 19:39:41 -0300

    Requisito 5

--- src/pages/productList.js
+++ src/pages/productList.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import CategoryList from '../components/CategoryList';
-import { getCategories } from '../services/api';
+import { getCategories, getProductsFromCategoryAndQuery } from '../services/api';
 
 class productList extends React.Component {
   constructor() {
     super();
     this.state = {
       categoryList: [],
+      querry: '',
+      productResults: [],
     };
   }
 
@@ -22,8 +24,24 @@ class productList extends React.Component {
     });
   };
 
+  handleChange = async ({ target: { value } }) => {
+    this.setState({
+      querry: value,
+    });
+  };
+
+  handleClick = async () => {
+    const { querry } = this.state;
+    const productArray = await getProductsFromCategoryAndQuery('', querry);
+    const productResults = productArray.results;
+    console.log(productResults);
+    this.setState({
+      productResults,
+    });
+  };
+
   render() {
-    const { categoryList } = this.state;
+    const { categoryList, querry, productResults } = this.state;
     return (
       <div>
         <Link to="/carrinho" data-testid="shopping-cart-button">Carrinho de compras</Link>
@@ -31,6 +49,39 @@ class productList extends React.Component {
           Digite algum termo de pesquisa ou escolha uma categoria.
         </p>
         <CategoryList categoryList={ categoryList } />
+
+        <label htmlFor="query-input">
+          <input
+            data-testid="query-input"
+            id="query-input"
+            type="text"
+            value={ querry }
+            onChange={ (event) => this.handleChange(event) }
+          />
+        </label>
+        <button
+          type="button"
+          data-testid="query-button"
+          onClick={ this.handleClick }
+        >
+          Buscar
+
+        </button>
+        <div>
+          {productResults.length === 0 && <p>Nenhum produto foi encontrado</p>}
+          {
+            productResults.map((item) => (
+              <div data-testid="product" key={ item.id }>
+                <p>{item.title}</p>
+                <img src={ item.thumbnail } alt={ item.title } />
+                <p>
+                  R$:
+                  {item.price}
+                </p>
+              </div>
+            ))
+          }
+        </div>
       </div>
     );
   }
